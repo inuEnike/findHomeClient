17f2ec9f460162e740a253f56c5554db07bfd960
Inu Enike <inuenike@gmail.com>
2025-09-15 04:43:55 +0100

    added the mobile nav functionality

--- js/index.js
+++ js/index.js
@@ -1,2 +1,7 @@
-let checkRoute = window.location.href;
+const hamburger = document.querySelector(".toggle");
+const navItems = document.querySelector(".mobile-nav");
 
+const toggleNav = () => {
+  navItems.classList.toggle("navToggle");
+  console.log("clicked");
+};
